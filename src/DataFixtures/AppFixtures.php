3ea36993b0c8e80feaa6cc0d2f6bafa0b5e0cecb
ABDOULHAKIN Mohamed <abdoulhakinmog@gmail.com>
2023-05-25 09:44:59 +0200

    First commit

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Post;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        for($i=0;$i<500;$i++)
+        {
+            $post = new Post();
+            $post->setTitle('Article'.$i);
+            $post->setContent('Contenu de mon premier article');
+            $post->setCreatedAt(new \DateTimeImmutable());
+
+            $manager->persist($post);
+        }
+        
+        $manager->flush();
+    }
+}
